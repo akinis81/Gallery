7d3568bede365b370e58b3ee6bda8e3d43970f52
akinis81 <66510705+akinis81@users.noreply.github.com>
2020-11-04 13:19:19 +0200

    formating fixed

--- js/Logic.js
+++ js/Logic.js
@@ -1,43 +1,43 @@
 class Logic {
-    constructor(gallerydata){
+    constructor(gallerydata) {
         this.gallerydata = gallerydata;
         this.ClickedItemGlobal;
         this.imagesCount;
         this.init();
     }
-    
-    init(){
+
+    init() {
         this.generateHtml();
         this.modalImage();
         this.imageSearch();
     }
 
-    generateHtml(){
-        
+    generateHtml() {
+
         let items = [];
         let count = 0;
         for (const iterator of this.gallerydata) {
             for (const item of iterator.categories) {
-                items[count++]= item.toLowerCase(); 
+                items[count++] = item.toLowerCase();
             }
         }
         let unique = [...new Set(items)];
-        unique = unique.sort(); 
+        unique = unique.sort();
         let filterHtml = '<div class="filterItem active"  data-categories="all">All</div>';
-            filterHtml += `<div class="dot">*</div>`
+        filterHtml += `<div class="dot">*</div>`
         for (let iterator = 0; iterator < unique.length; iterator++) {
-            filterHtml +=  `<div class="filterItem"  data-categories="${unique[iterator]}">${unique[iterator].charAt(0).toUpperCase() + unique[iterator].slice(1)}</div>` 
-            if (iterator < unique.length-1){
+            filterHtml += `<div class="filterItem"  data-categories="${unique[iterator]}">${unique[iterator].charAt(0).toUpperCase() + unique[iterator].slice(1)}</div>`
+            if (iterator < unique.length - 1) {
                 filterHtml += `<div class="dot">*</div>`
             }
 
         }
-        document.querySelector('.filter').insertAdjacentHTML('afterbegin',filterHtml);
+        document.querySelector('.filter').insertAdjacentHTML('afterbegin', filterHtml);
 
 
-        let imagehtml ='';
-            for (let i = 0; i < this.gallerydata.length ; i++) {
-                imagehtml +=   `<div class="primary">
+        let imagehtml = '';
+        for (let i = 0; i < this.gallerydata.length; i++) {
+            imagehtml += `<div class="primary">
                                     <div class="single">
                                         <img class="image" src="./img/${this.gallerydata[i].photo}" alt="Gallery photo" data-image="${i}">
                                     </div>
@@ -50,16 +50,16 @@ class Logic {
                                         </ul>
                                     </div>
                                 </div>`
-            }
+        }
 
 
 
-        document.querySelector('.list').insertAdjacentHTML('afterbegin',imagehtml);
+        document.querySelector('.list').insertAdjacentHTML('afterbegin', imagehtml);
         const singlePhoto = document.querySelectorAll('.primary');
         const categoriesFilter = document.querySelectorAll('.filterItem');
 
         for (let j = 0; j < categoriesFilter.length; j++) {
-            categoriesFilter[j].addEventListener('click', () =>{
+            categoriesFilter[j].addEventListener('click', () => {
                 const ClickedItem = categoriesFilter[j].dataset.categories;
                 this.ClickedItemGlobal = ClickedItem;
                 this.imageSearch();
@@ -68,59 +68,59 @@ class Logic {
                 activeFilterItem.classList.remove('active');
                 categoriesFilter[j].classList.add('active');
 
-                    for (let x = 0; x < this.gallerydata.length; x++) {
-                        if (ClickedItem === 'all') {
-                            singlePhoto[x].classList.remove('hidden')    
-                        } else {
+                for (let x = 0; x < this.gallerydata.length; x++) {
+                    if (ClickedItem === 'all') {
+                        singlePhoto[x].classList.remove('hidden')
+                    } else {
                         singlePhoto[x].classList.add('hidden')
-                            for (let index = 0; index < this.gallerydata[x].categories.length; index++) {
-                                if ( this.gallerydata[x].categories[index].toLowerCase() === ClickedItem) {
-                                    singlePhoto[x].classList.remove('hidden');
-                                }
+                        for (let index = 0; index < this.gallerydata[x].categories.length; index++) {
+                            if (this.gallerydata[x].categories[index].toLowerCase() === ClickedItem) {
+                                singlePhoto[x].classList.remove('hidden');
                             }
                         }
-                    }   
+                    }
+                }
             });
         }
-       
+
     }
-    modalImage(){
+    modalImage() {
         const image = document.querySelectorAll('.image')
         const text = document.querySelectorAll('.text')
-        const myModal = document.getElementById('myModal'); 
+        const myModal = document.getElementById('myModal');
         let modalImage = document.getElementById('modalImage');
         let activeImage = 0;
         for (let j = 0; j < image.length; j++) {
-            text[j].addEventListener('click', ()=>{
-               myModal.style.display = 'block';
-               modalImage.src = `./img/${this.gallerydata[j].photo}`;  
-               activeImage = image[j].dataset.image;
+            text[j].addEventListener('click', () => {
+                myModal.style.display = 'block';
+                modalImage.src = `./img/${this.gallerydata[j].photo}`;
+                activeImage = image[j].dataset.image;
             })
-            
+
         }
 
         const closeBtn = document.querySelector('.close')
-        closeBtn.addEventListener('click', ()=>{
+        closeBtn.addEventListener('click', () => {
             myModal.style.display = 'none';
-        }) 
+        })
 
         const right = document.querySelector('.right')
-        right.addEventListener('click', ()=>{
-            if(this.imagesCount.length < 1 || this.imagesCount == undefined){
-                this.ClickedItemGlobal ='all';
+        right.addEventListener('click', () => {
+            if (this.imagesCount.length < 1 || this.imagesCount == undefined) {
+                this.ClickedItemGlobal = 'all';
                 this.imageSearch();
             }
             let index = 0;
             for (let b = 0; b < this.imagesCount.length; b++) {
-                if (parseInt(activeImage)===this.imagesCount[b]) {
-                index = b;
+                if (parseInt(activeImage) === this.imagesCount[b]) {
+                    index = b;
                 }
             }
             index++;
-            if ( index < this.imagesCount.length){
+            if (index < this.imagesCount.length) {
                 activeImage = this.imagesCount[index];
                 modalImage.src = `./img/${this.gallerydata[activeImage].photo}`;
-            } else{
+            } else {
                 index = 0;
                 activeImage = this.imagesCount[index];
                 modalImage.src = `./img/${this.gallerydata[activeImage].photo}`
@@ -129,43 +129,43 @@ class Logic {
         })
 
         const left = document.querySelector('.left')
-        left.addEventListener('click', ()=>{
+        left.addEventListener('click', () => {
 
-            if(this.imagesCount.length < 1 || this.imagesCount == undefined){
-                this.ClickedItemGlobal ='all';
+            if (this.imagesCount.length < 1 || this.imagesCount == undefined) {
+                this.ClickedItemGlobal = 'all';
                 this.imageSearch();
             }
             let index = 0;
             for (let b = 0; b < this.imagesCount.length; b++) {
-                if (parseInt(activeImage)===this.imagesCount[b]) {
-                index = b;
+                if (parseInt(activeImage) === this.imagesCount[b]) {
+                    index = b;
                 }
             }
             index--;
-            if ( index > -1){
+            if (index > -1) {
                 activeImage = this.imagesCount[index];
                 modalImage.src = `./img/${this.gallerydata[activeImage].photo}`;
-            } else{
-                index = this.imagesCount.length-1;
+            } else {
+                index = this.imagesCount.length - 1;
                 activeImage = this.imagesCount[index];
                 modalImage.src = `./img/${this.gallerydata[activeImage].photo}`
             }
         })
-        
+
     }
 
-    imageSearch(){
+    imageSearch() {
         this.imagesCount = [];
         for (let t = 0; t < this.gallerydata.length; t++) {
-               for (let l = 0; l <this.gallerydata[t].categories.length; l++) {
-                if (this.gallerydata[t].categories[l].toLowerCase() === this.ClickedItemGlobal ){
+            for (let l = 0; l < this.gallerydata[t].categories.length; l++) {
+                if (this.gallerydata[t].categories[l].toLowerCase() === this.ClickedItemGlobal) {
                     this.imagesCount.push(t);
-                } 
+                }
             }
-            if (this.ClickedItemGlobal === 'all'){
+            if (this.ClickedItemGlobal === 'all') {
                 this.imagesCount.push(t);
             }
         }
     }
 }
-export {Logic}  
+export { Logic }  
